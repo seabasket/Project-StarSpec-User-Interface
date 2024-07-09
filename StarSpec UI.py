5b037c9b10f623a326057932edd3cdae68a5f3bd
Alejandro Olivo <131506955+AlejandrOlivo@users.noreply.github.com>
2024-07-09 19:36:36 -0400

    Update StarSpec UI.py

--- StarSpec UI.py
+++ StarSpec UI.py
@@ -60,31 +60,6 @@ switch2 = ctk.CTkButton(frame2,
 switch2.pack(expand=1)
 switch2.place(x=10, y=10)
 
-#zoom in and out buttons
-# def zoomIn():
-#     print("in")
-# def zoomOut():
-#     print("out")
-
-# zoomIn_image = ImageTk.PhotoImage(Image.open("Zoom-In.png").resize((30, 30)))
-# zoomOut_image = ImageTk.PhotoImage(Image.open("Zoom-Out.png").resize((30, 30)))
-
-# zoomIn_button = ctk.CTkButton(frame1, image=zoomIn_image,
-#                 text="",
-#                 command=zoomIn,
-#                 height=30, width=30)
-# zoomIn_button.pack(anchor="nw", expand=1)
-# zoomIn_button.place(x=500, y=10)
-
-# zoomOut_button = ctk.CTkButton(frame1, image=zoomOut_image,
-#                 text="",
-#                 command=zoomOut,
-#                 height=30, width=30)
-# zoomOut_button.pack(anchor="nw", expand=1)
-# zoomOut_button.place(x=645, y=50)
-
-#gain
-
 #exposure time feature
 exposure_time = ctk.CTkEntry(frame1,
                             font=("Helvetica", 18),
@@ -151,14 +126,6 @@ def submit():
     else:
         test_label2.configure(text=f"Temperature is set at {temperature.get()} °C")
         print(f"Temperature is {temperature.get()} °C")
-    # if len(exposure_time.get()) == 0:
-    #     time = int(exposure_time.get())
-    #     print(time)
-    #     #exposure_time.get() == 0
-    # if len(temperature.get()) == 0:
-    #     temperature.get() == 0
-    #     print(temperature.get())
-    #     print("no temp receiveed")
 
 submit = ctk.CTkButton(frame1,
                 text="Submit", font=("Helvetica", 18), text_color="white",
@@ -225,73 +192,6 @@ save_image = ctk.CTkButton(frame2,
                 anchor="ne")
 save_image.pack(padx=10, pady=10, anchor="nw", expand=1)
 save_image.place(x=720, y=10)
-
-#live view feature
-def liveView():
-    print("Currently viewing the live feed")
-
-live_view = ctk.CTkButton(frame2,
-                text="Live View", font=("Helvetica", 18), text_color="white",
-                fg_color="black", bg_color="transparent", hover_color="dark grey",
-                command=liveView,
-                height=30, width=80,
-                anchor="ne")
-live_view.pack(padx=10, pady=10, anchor="nw", expand=1)
-live_view.place(x=620, y=10)
-
-# def function(choice):
-#     if choice == "Function 1":
-
-#         print("Function 1 was selected")
-#     elif choice == "Function 2":
-#         print("Function 2 was selected")
-#     elif choice == "Function 3":
-#         print("Function 3 was selected")
-#     elif choice == "Function 4":
-#         print("Function 4 was selected")
-
-# #dropdown menu 
-# options = ["Function 1", "Function 2", "Function 3", "Function 4"]
-
-# #dropdown menu
-# dropMenu = ctk.CTkOptionMenu(frame2, values=options, command=function,
-#                             anchor="nw",
-#                             width=100, height=25,
-#                             corner_radius=10,
-#                             button_color="white",
-#                             text_color="black", font=("Helvetica", 14), fg_color="white",
-#                             dropdown_fg_color="white", dropdown_hover_color="light grey", dropdown_font=("Helvetica", 14))
-# dropMenu.pack(padx=10, pady=10, expand=1, anchor="nw")
-
-# def enter_pressed(event):
-#     search.get()
-#     print(search.get)
-
-# #search bar
-# search = ctk.CTkEntry(frame1, placeholder_text="Search...",
-#                     font=("Helvetica", 14),
-#                     corner_radius=10)
-# search.pack(padx=150, pady=30, expand=1, anchor="nw")
-
-#open a website
-# webview.create_window("Stellarium", "https://spacetelescopelive.org/webb?obsId=01HZ086YP1Z8AY11WPCA54C349") 
-# webview.start()
-
-
-
-#Title
-# title = ctk.CTkLabel(root, text = "Project StarSpec", font = ("Times New Roman", 40), fg_color = "#000000", bg_color= "#FFFFFF")
-# title.grid(row = 0, column = 0,
-#             padx = 250, pady = 5,
-#             sticky = "ew")
-# # title_frame = Frame(root, bg = "#000000")
-
-# #Search Bar
-# text = tkinter.StringVar()
-# search = ctk.CTkEntry(root, width = 150, height = 15, placeholder_text = "Search", textvariable = text)
-# search.grid(row = 0, column = 10,
-#             padx = 575,
-#             sticky = "ew")
 
 frame1.tkraise()
 #Run app

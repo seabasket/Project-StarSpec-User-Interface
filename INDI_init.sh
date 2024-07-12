79274f361d9a2f71e549083e64698583f08e9217
Alejandro Olivo <131506955+AlejandrOlivo@users.noreply.github.com>
2024-07-12 17:21:15 -0400

    Update INDI_init.sh

--- INDI_init.sh
+++ INDI_init.sh
@@ -3,5 +3,5 @@
 #initiate INDI virtual environment
 source ~/venv_indi_pylibcamera/bin/activate
 
-#init picamera, mount, and ZWO camera (will run continuously)
+#init picamera2, mount, and ZWO camera (will run continuously)
 indiserver -v indi_pylibcamera ./indi/build/drivers/telescope/indi_celestron_gps indi_asi_ccd
